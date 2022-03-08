5f14583b22e3c319e96f813ccd7cb58be078ae77
Mason Phillips <math.matrix@icloud.com>
2022-03-08 14:11:16 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument4`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument1`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan2`, function (sprite, location) {
+    info.changeScoreBy(-1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan1`, function (sprite, location) {
+    info.changeScoreBy(-1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`instrument0`, function (sprite, location) {
+    info.changeScoreBy(1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`fan3`, function (sprite, location) {
+    info.changeScoreBy(-1)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`exit`, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+scene.setBackgroundColor(13)
+tiles.setTilemap(tilemap`level1`)
+let mySprite = sprites.create(assets.image`rockstar`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.placeOnRandomTile(mySprite, assets.tile`stage`)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(30)
